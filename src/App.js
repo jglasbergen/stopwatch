31d157cc80a8e78f2b5cc73f12ccddcde8750fbe
Jaap Glasbergen <j.glasbergen@outlook.com>
2019-05-24 17:43:52 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PageVisibility from "react-page-visibility";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stopwatch</h1>
+      <Stopwatch />
     </div>
   );
+}
+
+class Stopwatch extends React.Component {
+  state = {
+    status: false,
+    runningTime: 0
+  };
+
+  handleVisibilityChange = isVisible => {
+    if (isVisible && this.state.status === true) {
+      const startTime = Date.now() - this.state.runningTime;
+      this.timer = setInterval(() => {
+        this.setState({ runningTime: Date.now() - startTime });
+      });
+    } else {
+      clearInterval(this.timer);
+    }
+    // this.setState({ status: !isVisible });
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  handleClick = () => {
+    this.setState(state => {
+      if (state.status) {
+        clearInterval(this.timer);
+      } else {
+        const startTime = Date.now() - this.state.runningTime;
+        this.timer = setInterval(() => {
+          this.setState({ runningTime: Date.now() - startTime });
+        });
+      }
+      return { status: !state.status };
+    });
+  };
+
+  handleReset = () => {
+    clearInterval(this.timer);
+    this.setState({ runningTime: 0, status: false });
+  };
+
+  time = ms => {
+    return new Date(ms).toISOString().slice(11, -1);
+  };
+  render() {
+    const { status, runningTime } = this.state;
+    return (
+      <PageVisibility onChange={this.handleVisibilityChange}>
+        <div>
+          <p>{this.time(runningTime)}ms</p>
+          <button onClick={this.handleClick}>
+            {status ? "Stop" : "Start"}
+          </button>
+          <button onClick={this.handleReset}>Reset</button>
+        </div>
+      </PageVisibility>
+    );
+  }
 }
 
 export default App;
